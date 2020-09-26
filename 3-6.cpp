dfc6c8063928a277250714a4cd41a192edd448de
jialbreak <1348929038@qq.com>
2020-09-27 00:32:50 +0800

    section3
    section3

---
+++ 3-6.cpp
@@ -0,0 +1,20 @@
+#define abs(x) ((x) < 0 ? -(x):(x))
+
+/*itoa:convert n to charcters in s,w characters wide */
+
+void itoa(int n, char s[], int w) {
+	int i, sign;
+	void reverse(char s[]);
+
+	sign = n;
+	i = 0;
+	do {
+		s[i++] = abs(n % 10) + '0';
+	} while ((n /= 10) != 0);
+	if (sign < 0)
+		s[i++] = '-';
+	while (i < w)
+		s[i++] = ' ';
+	s[i] = '\0';
+	reverse(s);
+}
